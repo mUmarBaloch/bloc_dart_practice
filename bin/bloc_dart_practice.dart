ad16791b94310113c632a694b75f9a80dceae9aa
Umar Baloch <umarbaloch262@gmail.com>
2025-04-18 17:56:33 +0500

    initial

---
+++ bin/bloc_dart_practice.dart
@@ -0,0 +1,5 @@
+import 'package:bloc_dart_practice/bloc_dart_practice.dart' as bloc_dart_practice;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bloc_dart_practice.calculate()}!');
+}
---
+++ test/bloc_dart_practice_test.dart
@@ -0,0 +1,8 @@
+import 'package:bloc_dart_practice/bloc_dart_practice.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
