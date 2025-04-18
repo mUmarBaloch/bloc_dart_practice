a2a88f984a57fb7a6423a346a2abc1b0c7b88f9f
Umar Baloch <umarbaloch262@gmail.com>
2025-04-18 18:15:43 +0500

    overriding the onChange and printing the change

--- bin/bloc_dart_practice.dart
+++ bin/bloc_dart_practice.dart
@@ -2,14 +2,14 @@ import 'package:bloc_dart_practice/counter_cubit.dart' ;
 
 void main(List<String> arguments) async{
   final counterCubit = CounterCubit();
-  final subscribstion = counterCubit.stream.listen(print);
+ // final subscribstion = counterCubit.stream.listen(print);
  for(int i = 0 ; i <=5; i++){
   counterCubit.increament();
 
   await Future.delayed(Duration(seconds: 1));
  }
 await Future.delayed(Duration.zero);
-await subscribstion.cancel();
+//await subscribstion.cancel();
 await counterCubit.close();
  
 }
--- lib/counter_cubit.dart
+++ lib/counter_cubit.dart
@@ -3,4 +3,10 @@ import 'package:bloc/bloc.dart';
 class CounterCubit extends Cubit<int> {
   CounterCubit() :super(0);
   void increament() => emit(state+1);
+
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    print(change);
+  }
 }
