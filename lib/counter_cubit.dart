98f9ffb5486bdb12fbda9d3727f994f3ece1ff50
Umar Baloch <umarbaloch262@gmail.com>
2025-04-24 00:01:28 +0500

    decreament emit

--- lib/counter_cubit.dart
+++ lib/counter_cubit.dart
@@ -3,7 +3,7 @@ import 'package:bloc/bloc.dart';
 class CounterCubit extends Cubit<int> {
   CounterCubit() :super(0);
   void increament() => emit(state+1);
-  void decreament() => 2;
+  void decreament() => emit(state-1);
   @override
   void onChange(Change<int> change) {
     super.onChange(change);
